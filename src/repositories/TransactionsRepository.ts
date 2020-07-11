e20a1f7e03b6f5eb5803a32f7413bc461c4e50c2
IceHower <vctelles09@gmail.com>
2020-07-11 00:04:10 -0300

    Challenge completed

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,5 +1,12 @@
 import Transaction from '../models/Transaction';
 
+interface CreateTransactionDTO {
+  // Cria um Data Transfer Object
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 interface Balance {
   income: number;
   outcome: number;
@@ -14,15 +21,50 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
+  // Cria uma função que ira buscar o income, outcome do array de transactions somando pela função reduce
   public getBalance(): Balance {
-    // TODO
+    const { income, outcome } = this.transactions.reduce(
+      (accumulator: Balance, transaction: Transaction) => {
+        switch (
+          transaction.type // Fez um switch para verificar o tipo da transaction.
+        ) {
+          case 'income':
+            accumulator.income += transaction.value;
+            break;
+          case 'outcome':
+            accumulator.outcome += transaction.value;
+            break;
+          default:
+            // Não faz nada por padrão
+            break;
+        }
+        return accumulator;
+      },
+      {
+        // Inicializa os valores como 0
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
+    const total = income - outcome; // Depois calculamos o total
+    return { income, outcome, total }; // E retornamos um objeto com income, outcome, total.
   }
 
-  public create(): Transaction {
-    // TODO
+  // Cria uma função para verificar se o valor é valido
+  public isValidTotal(value: number): boolean | null {
+    const balance = this.getBalance();
+    const isValid = balance.total > value;
+    return isValid || null;
+  }
+
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type }); // Instancia uma nova transação.
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface RequestDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,22 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: RequestDTO): Transaction {
+    const isBalanceValid = this.transactionsRepository.isValidTotal(value); // passa o valor para a função pra verificar se e valido ou não
+
+    if (type === 'outcome' && !isBalanceValid) {
+      // Se o tipo for outcome e a função não for valida retorna um erro.
+      throw Error('Insuficient founds');
+    }
+
+    const transaction = this.transactionsRepository.create({
+      // Cria a transaction
+      title,
+      value,
+      type,
+    });
+
+    return transaction; // Retorna a transaction
   }
 }
 
